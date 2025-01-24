bdbf0f6d3e1d778be6991dafb86676dd02f7f1cd
MrTGK0386 <71849240+MrTGK0386@users.noreply.github.com>
2025-01-24 17:39:32 +0100

    ADD :
    Ajout palettes de couleurs pour les disques

--- Assets/Scripts/Utils/RandomColors.cs
+++ Assets/Scripts/Utils/RandomColors.cs
@@ -6,14 +6,19 @@ using UnityEngine;
 public class RandomColors : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+    public Color[] colors;
     void Start()
     {
+        PopulateColorArray();
         GenerateColor();
     }
     
     public void GenerateColor()
     {
-        Color newColor = Random.ColorHSV();
+        int rand = Random.Range(0, colors.Length);
+        Color newColor = colors[rand];
+        
         
         Debug.Log(newColor.ToString());
         
@@ -27,4 +32,17 @@ public class RandomColors : MonoBehaviour
         Debug.Log(localMaterial.name);
         localMaterial.color = color; 
     }
+
+    void PopulateColorArray()
+    { 
+        colors[0] = new Color(94f / 255f, 148f / 255f, 224f / 255f);
+        colors[1] = new Color(243f / 255f, 155f / 255f, 186f / 255f);
+        colors[2] = new Color(125f / 255f, 226f / 255f, 245f / 255f);
+        colors[3] = new Color(255f / 255f, 203f / 255f, 134f / 255f);
+        colors[4] = new Color(201f / 255f, 154f / 255f, 248f / 255f);
+        colors[5] = new Color(169f / 255f, 227f / 255f, 90f / 255f);
+
+    }
+    
+    
 }
