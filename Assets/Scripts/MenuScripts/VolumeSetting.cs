3ca0b40bf7f7be731c5e032d98aaf055a95fe493
MrTGK0386 <71849240+MrTGK0386@users.noreply.github.com>
2025-01-25 08:02:16 +0100

    ADD :
    Slider pour les sons, c'est pété viens fabio

--- Assets/Scripts/MenuScripts/VolumeSetting.cs
+++ Assets/Scripts/MenuScripts/VolumeSetting.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
@@ -8,4 +9,41 @@ public class VolumeSetting : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("music"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
+    
+    public void SetMusicVolume()
+    {
+        float musicVolume = musicSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(musicVolume) * 20);
+        PlayerPrefs.SetFloat("music", musicVolume);
+    }
+    public void SetSFXVolume()
+    {
+        float sfxVolume = sfxSlider.value;
+        myMixer.SetFloat("sfx", Mathf.Log10(sfxVolume) * 20);
+        PlayerPrefs.SetFloat("sfx", sfxVolume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("music");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfx");
+
+        SetMusicVolume();
+        SetSFXVolume();
+    }
 }
